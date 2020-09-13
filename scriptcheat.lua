6001254cc86fc1e49226b88fd56ed0fe373376e1
SharKKProd <66116801+SharKKProd@users.noreply.github.com>
2020-09-13 08:52:51 +0500

    Update scriptcheat.lua

--- scriptcheat.lua
+++ scriptcheat.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/legendsofspeedscript/master/cheat.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/SharKKProd/speedhacksimulatorroblox/master/cheat.lua'),true))()
